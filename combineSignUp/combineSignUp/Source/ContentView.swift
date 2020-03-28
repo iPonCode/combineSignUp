490fa041e868e4720574f86b4da873c7d331920f
Simon <simon@aparicio.org>
2020-03-29 00:05:31 +0100

    initial commit

---
+++ combineSignUp/combineSignUp/Source/ContentView.swift
@@ -0,0 +1,20 @@
+//  ContentView.swift
+//  combineSignUp
+//
+//  Created by Simón Aparicio on 28/03/2020.
+//  Copyright © 2020 iPon.es. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
